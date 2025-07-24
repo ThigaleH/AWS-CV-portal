bcf2faeace3c3e95c511342f81d973a9891238a8
ThigaleH <thigaleharshada@gmail.com>
2025-07-24 17:58:24 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import CVUploadForm from './components/CVUploadForm';
 import LoginForm from './components/LoginForm';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Track login state
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div className="App">
       <h1>CV Upload Portal</h1>
-      <LoginForm />
-      <CVUploadForm />
+      {!isLoggedIn ? (
+        <LoginForm onLoginSuccess={handleLoginSuccess} />
+      ) : (
+        <CVUploadForm />
+      )}
     </div>
   );
 }
