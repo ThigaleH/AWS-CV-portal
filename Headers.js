1d8af5be4d7e93aaf96bcba9123c184a91a14872
ThigaleH <thigaleharshada@gmail.com>
2025-07-24 17:48:11 +0530

    Create Headers.js

---
+++ Headers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <header style={{ textAlign: 'center', padding: '1rem' }}>
+    <h1>CV Upload Portal</h1>
+    <p>Upload, Parse & Manage Candidate Resumes</p>
+  </header>
+);
+
+export default Header;
